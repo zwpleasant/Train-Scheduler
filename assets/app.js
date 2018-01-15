0b04626ffea538dc5a02cb45146eab14c124bdbc
Pleasant <zpleasant@anl.gov>
2018-01-15 14:55:18 -0600

    added js functionality to calculate Next Arrival/Minutes Away fields in database

--- assets/app.js
+++ assets/app.js
@@ -20,14 +20,14 @@ $("#add-train-btn").on("click", function(event) {
   // create variables with the user input from form
   var trnName = $("#train-name-input").val().trim();
   var trnDest = $("#destination-input").val().trim();
-  // var firstTrnTime = $("#first-train-input").val().trim();
+  var firstTrnTime = $("#first-train-input").val().trim();
   var trnFreq = $("#frequency-input").val().trim();
 
   // create a temporary object for holding the new train data
   var newTrn = {
     name: trnName,
     destination: trnDest,
-    // firstTime: firstTrnTime,
+    firstTime: firstTrnTime,
     frequency: trnFreq
   };
 
@@ -37,34 +37,47 @@ $("#add-train-btn").on("click", function(event) {
   // console log the values that were just pushed to the database
   console.log(newTrn.name);
   console.log(newTrn.destination);
-  // console.log(newTrn.firstTime);
+  console.log(newTrn.firstTime);
   console.log(newTrn.frequency);
 
   // clear the form values after values have been stored
   $("#train-name-input").val("");
   $("#destination-input").val("");
-  // $("#first-train-input").val("");
+  $("#first-train-input").val("");
   $("#frequency-input").val("");
 });
 
-// Create a firebase event for adding the data from the new trains and then populating them in the DOM.
+// create a firebase event for adding the data from the new trains and then populating them in the DOM.
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   console.log(childSnapshot.val());
 
-  // Store snapshot changes in variables
+  // store snapshot changes in variables
   var trnName = childSnapshot.val().name;
   var trnDest = childSnapshot.val().destination;
-  // var firstTrnTime = childSnapshot.val().firstTime;
+  var firstTrnTime = childSnapshot.val().firstTime;
   var trnFreq = childSnapshot.val().frequency;
 
-  // Log the values
+  // log the values
   console.log(trnName);
   console.log(trnDest);
-  // console.log(firstTrnTime);
+  console.log(firstTrnTime);
   console.log(trnFreq);
 
-  // Need to add in steps that will calculate the date/time fields!
+  // process for calculating the Next Arrival and Minutes Away fields...
+  // make sure the first train time is after the eventual current time
+  var firstTrnTimeConv = moment(firstTrnTime, "hh:mm a").subtract(1, "years");
+  // store variable for current time
+  var currentTime = moment().format("HH:mm a");
+  console.log("Current Time:" + currentTime);
+  // store variable for difference of current time and first train time
+  var trnTimeCurrentTimeDiff = moment().diff(moment(firstTrnTimeConv), "minutes");
+  // store the time left
+  var timeLeft = trnTimeCurrentTimeDiff % trnFreq;
+  // calculate and store the minutes until next train arrives
+  var minutesAway = trnFreq - timeLeft;
+  // calculate the next arriving train
+  var nextArrival = moment().add(nextArrival, "minutes").format("hh:mm a");
 
-  // Add the data into the DOM/html
-  $("#train-table > tbody").append("<tr><td>" + trnName + "</td><td>" + trnDest + "</td><td>" + trnFreq + "</td></tr>");
+  // add the data into the DOM/html
+  $("#train-table > tbody").append("<tr><td>" + trnName + "</td><td>" + trnDest + "</td><td>" + trnFreq + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
 });
